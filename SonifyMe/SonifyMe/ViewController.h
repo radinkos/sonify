0061d6da419b8e216356059c5d043a005f11f7ec
Kalpakova, Radina <kalpakov@amazon.com>
2014-10-11 10:51:31 +0100

    Created an empty project

---
+++ SonifyMe/SonifyMe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SonifyMe
+//
+//  Created by Kalpakova, Radina on 11/10/2014.
+//  Copyright (c) 2014 Sonify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SonifyMe/SonifyMeTests/SonifyMeTests.m
@@ -0,0 +1,34 @@
+//
+//  SonifyMeTests.m
+//  SonifyMeTests
+//
+//  Created by Kalpakova, Radina on 11/10/2014.
+//  Copyright (c) 2014 Sonify. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SonifyMeTests : XCTestCase
+
+@end
+
+@implementation SonifyMeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
